4145e1cb9355b6ba9a62ec4248def4aaa182d73a
fathimathsafwana <fathimathsafwanatayattuchira@gmail.com>
2026-02-25 11:53:08 +0530

    Fix typos in output messages for string manipulation

--- StringManipulation.java
+++ StringManipulation.java
@@ -9,7 +9,7 @@ public class StringManipulation {
 		String str=sc.nextLine();
 		System.out.println("Length:"+str.length());
 		System.out.println("Uppercase:"+str.toUpperCase());
-		System.out.println("Lwercase:"+str.toLowerCase());
+		System.out.println("Lowercase:"+str.toLowerCase());
 	String reversed="";
 	for(int i=str.length()-1;i>=0;i--) {
 		reversed+=str.charAt(i);
@@ -20,11 +20,11 @@ public class StringManipulation {
 	String str2=sc.nextLine();
 	System.out.println("Concatenated String:"+str.concat(str2));
 	if(str.equals(str2)) {
-		System.out.println("Both Strings are equal:");
+		System.out.println("Both Strings are equal");
 	
 	}
 	else {
-		System.out.println("Strings are not equal:");
+		System.out.println("Strings are not equal");
 	}
 	
 	sc.close();
